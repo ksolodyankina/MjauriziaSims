b0dd074955c7d554e97891b736e1846152206d45
katokey <ks.solodyankina@gmail.com>
2022-11-18 15:48:24 +0300

    Randomize goal page implemented

---
+++ MySimsSite/Controllers/RandomizerController.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Abstract;
+using System.Linq;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using WebUI.Models;
+
+namespace WebUI.Controllers
+{
+    public class RandomizerController : Controller
+    {
+        private readonly IGoalRepository _goalRepository;
+        private readonly IPreferenceRepository _preferenceRepository;
+        private readonly ICareerRepository _careerRepository;
+
+        public RandomizerController(
+            IGoalRepository goalRepository,
+            IPreferenceRepository preferenceRepository,
+            ICareerRepository careerRepository)
+        {
+            _goalRepository = goalRepository;
+            _preferenceRepository = preferenceRepository;
+            _careerRepository = careerRepository;
+        }
+
+        public ViewResult Goal()
+        {
+            return View(_goalRepository.Goals.ToList());
+        }
+    }
+}
